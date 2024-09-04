83e53575c11d831c7b96267308b07bcd56565fbe
shirshak182 <supadhayay@proventusanalytics.com>
2024-09-04 21:39:59 +0545

    Added: some code

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,55 +10,17 @@ import static org.junit.Assert.assertEquals;
  */
 public class MainTest {
 
-    public int binarySearch(int arr[], int low, int high, int x){
-        if (high >= low) {
-            int mid = low + (high - low) / 2;
-
-            // If the element is present at the
-            // middle itself
-            if (arr[mid] == x)
-                return mid;
-
-            // If element is smaller than mid, then
-            // it can only be present in left subarray
-            if (arr[mid] > x)
-                return binarySearch(arr, low, mid - 1, x);
-
-            // Else the element can only be present
-            // in right subarray
-            return binarySearch(arr, mid + 1, high, x);
-        }
-
-        // We reach here when element is not present
-        // in array
-        return -1;
+    @Test
+    public void testAdditionOperation() {
+        ArithmeticOperation add = (a, b) -> a + b;
+        assertEquals(String.valueOf(30), String.valueOf(add.operate(20, 10)), "20 + 10 should be 30");
     }
-    public void display(ArithmeticOperation operation){
-        System.out.println(operation.operate(20,10));
+
+    @Test
+    public void testSubtractionOperation() {
+        ArithmeticOperation subtract = (a, b) -> a - b;
+        assertEquals(String.valueOf(10), String.valueOf(subtract.operate(20, 10)), "20 - 10 should be 10");
     }
-    public static void main(String[] args) {
-        ArithmeticOperation add = (a,b)->a+b;
-        ArithmeticOperation multiply = (a,b)->a*b;
-        ArithmeticOperation divide = (a,b)->a/b;
-        ArithmeticOperation substract = (a,b)->a-b;
 
-        Main main = new Main();
-        main.display(add);
-        main.display(multiply);
-        main.display(divide);
-        main.display(substract);
-
-
-        int arr[] = { 2, 3, 4, 10, 40 };
-        int n = arr.length;
-        int x = 10;
-        int result = main.binarySearch(arr, 0, n - 1, x);
-        if (result == -1)
-            System.out.println(
-                    "Element is not present in array");
-        else
-            System.out.println(
-                    "Element is present at index " + result);
-    }
 
 }
